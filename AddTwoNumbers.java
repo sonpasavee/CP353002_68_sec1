ef0cf922a4fb7d048598f32bc74e0cad5de1797d
sonpasavee975 <sunpasavee@gmail.com>
2025-06-25 14:23:27 +0700

    AddTwoNumber
    sum number system

---
+++ AddTwoNumbers.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class AddTwoNumbers {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in); // ใช้สำหรับรับข้อมูลจากผู้ใช้
+
+        System.out.print("ป้อนตัวเลขตัวแรก: ");
+        int num1 = scanner.nextInt();
+
+        System.out.print("ป้อนตัวเลขตัวที่สอง: ");
+        int num2 = scanner.nextInt();
+
+        int sum = num1 + num2;
+
+        System.out.println("ผลรวมคือ: " + sum);
+    }
+}
--- hello.java
+++
@@ -1,9 +0,0 @@
-class hello {
-  public void hello {
-    System.out.print("hello my friends test1")
-  }
-
-  Int a = 10;
-  Int b = 20;
-  System.out.print(a+b)
-}
